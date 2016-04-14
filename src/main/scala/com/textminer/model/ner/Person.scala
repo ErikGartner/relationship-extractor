16a3facd49b4fa5ce97587feed7b5ee431e57d9c
AxelTLarsson <mail@axellarsson.nu>
2016-04-14 13:46:47 +0200

    Delete obsolete, overly complex and brilliant architecture

--- src/main/scala/com/textminer/model/ner/Person.scala
+++
@@ -1,6 +0,0 @@
-package com.textminer.model.ner
-
-/**
-  * Created by erik on 07/04/16.
-  */
-class Person(val name: String, val sentence: String)
--- src/main/scala/com/textminer/model/taggers/CoreNLPTagger.scala
+++
@@ -1,8 +0,0 @@
-package com.textminer.model.taggers
-
-/**
-  * Created by erik on 04/04/16.
-  */
-trait CoreNLPTagger extends POSTagger{
-
-}
