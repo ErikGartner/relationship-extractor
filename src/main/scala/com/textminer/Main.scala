869a8d98f108aa5b3cc96d682b188c058cd94524
Erik Gärtner <erik@gartner.io>
2016-04-14 13:49:35 +0200

    feat: Working relationship extraction for english

--- src/main/scala/com/textminer/Main.scala
+++ src/main/scala/com/textminer/Main.scala
@@ -6,21 +6,22 @@ import edu.stanford.nlp.dcoref.CorefCoreAnnotations.CorefChainAnnotation
 import edu.stanford.nlp.hcoref.CorefCoreAnnotations.CorefMentionsAnnotation
 import edu.stanford.nlp.ie.util.RelationTriple
 import edu.stanford.nlp.ling.CoreAnnotations._
-import edu.stanford.nlp.ling.{ CoreLabel, Sentence }
+import edu.stanford.nlp.ling.{CoreAnnotations, CoreLabel, Sentence}
 import edu.stanford.nlp.naturalli.NaturalLogicAnnotations.RelationTriplesAnnotation
 import edu.stanford.nlp.naturalli.OpenIE
-import edu.stanford.nlp.pipeline.{ Annotation, StanfordCoreNLP, StanfordCoreNLPClient }
+import edu.stanford.nlp.pipeline.{Annotation, StanfordCoreNLP, StanfordCoreNLPClient}
 import edu.stanford.nlp.semgraph.SemanticGraph
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation
 import edu.stanford.nlp.trees.Tree
 import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation
-import edu.stanford.nlp.pipeline.{ Annotation, StanfordCoreNLP }
+import edu.stanford.nlp.pipeline.{Annotation, StanfordCoreNLP}
 import edu.stanford.nlp.util.CoreMap
 import edu.stanford.nlp.hcoref.CorefCoreAnnotations
 import edu.stanford.nlp.hcoref.data.CorefChain
 import edu.stanford.nlp.hcoref.data.Mention
 
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 object Main extends App {
 
@@ -38,25 +39,53 @@ object Main extends App {
   // run all Annotators on this text
   pipeline.annotate(document)
 
-  // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+  // These are all corefchains in the document used to resolve corefs.
+  val chains = document.get(classOf[CorefCoreAnnotations.CorefChainAnnotation])
 
+  // We store relationsships as ("Subject" "Relationship" "Object")
+  val foundRelationships = mutable.Set[Tuple3[String, String, String]]()
+
+  // Get all sentences.
   val sentences = document.get(classOf[SentencesAnnotation]).asScala
-
   for (sentence: CoreMap <- sentences) {
 
+    // Get relationship triples in the sentence
     val oie = sentence.get(classOf[RelationTriplesAnnotation]).asScala
 
     for (triple: RelationTriple <- oie) {
       for (relation <- relations) {
+
+        // Check if the relation is one if the sought after
         if (triple.relationLemmaGloss.toLowerCase.contains(relation)) {
-          println(s"Sentence: ${triple} => ${triple.subjectLemmaGloss()} - ${triple.relationLemmaGloss()} - ${triple.objectLemmaGloss()}")
-          sentence.get(classOf[CorefMentionsAnnotation]).asScala foreach (s => println("\t" + s))
+
+          // Check if subject or object is an coreference
+          val subjectCorefId = triple.subjectHead().get(classOf[CorefCoreAnnotations.CorefClusterIdAnnotation])
+          val objectCorefId = triple.objectHead().get(classOf[CorefCoreAnnotations.CorefClusterIdAnnotation])
+
+          var subj = ""
+          var obj = ""
+
+          // resolve coreferences
+          if (subjectCorefId == null) {
+            subj = triple.subjectLemmaGloss()
+          }else{
+            subj = chains.get(subjectCorefId).getRepresentativeMention().mentionSpan
+          }
+
+          if (objectCorefId == null) {
+            obj = triple.objectLemmaGloss()
+          }else{
+            obj = chains.get(objectCorefId).getRepresentativeMention().mentionSpan
+          }
+
+          foundRelationships += Tuple3(subj, relation, obj)
+
         }
       }
     }
 
   }
 
-  document.get(classOf[CorefCoreAnnotations.CorefChainAnnotation]).values().asScala foreach (cc => println(s"\t ${cc}"))
+  println(foundRelationships)
 
 }
