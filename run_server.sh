45816e9320c59751545595bc9c0bcbbbf8955927
AxelTLarsson <mail@axellarsson.nu>
2016-05-09 16:00:59 +0200

    Use latest CoreNLP server (May 5th) in run_server.sh
    This resolves issue #6

--- run_server.sh
+++ run_server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 managedClasspath="./target/streams/runtime/managedClasspath/\$global/streams/export"
-unmanagedClasspath="./target/stream/runtime/unmanagedJars/\$global/streams/export"
+unmanagedClasspath="./target/streams/runtime/unmanagedJars/\$global/streams/export"
+
 # Using server defined in build.sbt
-java -mx4g -cp "$(< $managedClasspath)" edu.stanford.nlp.pipeline.StanfordCoreNLPServer
+# java -mx4g -cp "$(< $managedClasspath)" edu.stanford.nlp.pipeline.StanfordCoreNLPServer
 # TODO: Make this actually work:Using server from lib, i.e. latest from HEAD as of 5 May 2016
-# java -mx4g -cp "$(< $unmanagedJars):$(< $managedClasspath)" edu.stanford.nlp.pipeline.StanfordCoreNLPServer
+java -mx4g -cp "$(< ${unmanagedClasspath}):$(< ${managedClasspath})" edu.stanford.nlp.pipeline.StanfordCoreNLPServer
